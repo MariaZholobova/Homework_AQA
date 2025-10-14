851e900ec7bf50a3996f8a460b4b448ea061671b
MariaZholobova <jmi180895@gmail.com>
2025-10-14 13:05:41 +0300

    Выполнила ДЗ 2.3

---
+++ src/main/java/org/example/lesson3/Park.java
@@ -0,0 +1,29 @@
+package org.example.lesson3;
+/* Создать класс Park с внутренним классом, с помощью объектов которого можно хранить информацию об аттракционах,
+времени их работы и стоимости.*/
+
+import java.security.PublicKey;
+
+public class Park {
+    String name;
+    public Park (String name) {
+        this.name = name;
+    }
+    public static class Attractions {
+       String attractionName;
+       String openingHours;
+       double price;
+       public Attractions(String attractionName, String openingHours, double price) {
+           this.attractionName = attractionName;
+           this.openingHours = openingHours;
+           this.price = price;
+       }
+       public void printInfo() {
+           System.out.println("Аттракцион: " + attractionName);
+           System.out.println("Время работы: " + openingHours);
+           System.out.println("Цена: " + price);
+           System.out.println ();
+       }
+
+    }
+}
