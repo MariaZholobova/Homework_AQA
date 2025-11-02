49eb21c305019f935e79b44a3624baf54eea7acb
MariaZholobova <jmi180895@gmail.com>
2025-11-02 20:23:27 +0300

    Отредактировала отступы

--- src/main/java/org/example/FactorialCalculator.java
+++ src/main/java/org/example/FactorialCalculator.java
@@ -5,10 +5,10 @@ public class FactorialCalculator {
         if (n < 0) {
             throw new IllegalArgumentException("Число не может быть меньше нуля");
         }
-            long result = 1;
-            for (int i = 1; i <= n; i++) {
-                result *= i;
-            }
-            return result;
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
         }
+        return result;
     }
+}
