16e33c836450e3d3e35b16cb6479319f42a67c28
MariaZholobova <jmi180895@gmail.com>
2025-11-14 17:48:23 +0300

    Разделила большие методы на более маленькие

--- src/test/java/com/mts/pages/OnlinePaymentPage.java
+++ src/test/java/com/mts/pages/OnlinePaymentPage.java
@@ -1,5 +1,6 @@
 package com.mts.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,17 +11,15 @@ import java.time.Duration;
 
 public class OnlinePaymentPage {
     private WebDriver driver;
-
     private By acceptCookieButton = By.id("cookie-agree");
     /*Форма: Онлайн пополнение без комиссии*/
     private By blockTitle = By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]");
     private By visaLogo = By.xpath("//img[@alt = 'Visa']");
-    private By verifiedByVisaLogo = By.xpath("//img[@ alt = 'Verified By Visa']");
+    private By verifiedByVisaLogo = By.xpath("//img[@alt = 'Verified By Visa']");
     private By masterCardLogo = By.xpath("//img[@alt = 'MasterCard']");
     private By masterCardSecureCodeLogo = By.xpath("//img[@alt = 'MasterCard Secure Code']");
     private By belcartLogo = By.xpath("//img[@alt = 'Белкарт']");
     private By detailsLink = By.linkText("Подробнее о сервисе");
-
     /*Услуги связи */
     private By menu = By.cssSelector("button.select__header");
     private By option = By.xpath("//p[@class='select__option' and text()='Услуги связи']");
@@ -28,27 +27,23 @@ public class OnlinePaymentPage {
     private By fieldSum = By.id("connection-sum");
     private By fieldEmail = By.id("connection-email");
     private By continueButton = By.cssSelector("#pay-connection button[type='submit']");
-
     /*Домашний интернет*/
     private By option2 = By.xpath("//p[@class='select__option' and text()='Домашний интернет']");
     private By fieldPhoneNumber2 = By.id("internet-phone");
     private By fieldSum2 = By.id("internet-sum");
     private By fieldEmail2 = By.id("internet-email");
-
     /*Рассрочка*/
     private By option3 = By.xpath("//p[@class='select__option' and text()='Рассрочка']");
     private By fieldAccountNumber = By.id("score-instalment");
     private By fieldSum3 = By.id("instalment-sum");
     private By fieldEmail3 = By.id("instalment-email");
-
     /* Задолженность */
     private By option4 = By.xpath("//p[@class='select__option' and text()='Задолженность']");
     private By fieldAccountNumber2 = By.id("score-arrears");
     private By fieldSum4 = By.id("arrears-sum");
     private By fieldEmail4 = By.id("arrears-email");
-
+    /*Переход по ссылке*/
     private By headerText = By.xpath("//h3[contains(text(), 'Оплата банковской картой')]");
-
     /* Фрейм*/
     private By frame = By.cssSelector("iframe.bepaid-iframe");
     private By frameTitle = By.xpath("//div[contains(@class, 'pay-description__cost')]/span");
@@ -68,6 +63,7 @@ public class OnlinePaymentPage {
         this.driver = driver;
     }
 
+    @Step("Принять баннер cookie, если он видим")
     public void acceptCookieButtonIfVisible() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         try {
@@ -78,26 +74,49 @@ public class OnlinePaymentPage {
         }
     }
 
+    @Step("Получить название блока онлайн пополнения")
     public String getBlockTitleText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement title = wait.until(ExpectedConditions.visibilityOfElementLocated(blockTitle));
         return title.getText();
     }
 
-    public boolean arePaymentLogosDisplayed() {
+    @Step("Проверить отображение логотипа Visa на странице")
+    public boolean isPaymentLogoVisaDisplayed() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement visa = wait.until(ExpectedConditions.visibilityOfElementLocated(visaLogo));
-        WebElement verifiedByVisa = wait.until(ExpectedConditions.visibilityOfElementLocated(verifiedByVisaLogo));
-        WebElement masterCard = wait.until(ExpectedConditions.visibilityOfElementLocated(masterCardLogo));
-        WebElement masterCardSecureCode = wait.until(ExpectedConditions.visibilityOfElementLocated(masterCardSecureCodeLogo));
-        WebElement belcart = wait.until(ExpectedConditions.visibilityOfElementLocated(belcartLogo));
-        return visa.isDisplayed() &&
-                verifiedByVisa.isDisplayed() &&
-                masterCard.isDisplayed() &&
-                masterCardSecureCode.isDisplayed() &&
-                belcart.isDisplayed();
+        return visa.isDisplayed();
     }
 
+    @Step("Проверить отображение логотипа VerifiedByVisa на странице")
+    public boolean isPaymentLogoVerifiedByVisaDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement verifiedByVisa = wait.until(ExpectedConditions.visibilityOfElementLocated(verifiedByVisaLogo));
+        return verifiedByVisa.isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа MasterCard на странице")
+    public boolean isPaymentLogoMasterCardDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement masterCard = wait.until(ExpectedConditions.visibilityOfElementLocated(masterCardLogo));
+        return masterCard.isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа MasterCard Secure Code на странице")
+    public boolean isPaymentLogoMasterCardSecureCodeDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement masterCardSecureCode = wait.until(ExpectedConditions.visibilityOfElementLocated(masterCardSecureCodeLogo));
+        return masterCardSecureCode.isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа Belcart на странице")
+    public boolean isPaymentLogoBelcartDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement belcart = wait.until(ExpectedConditions.visibilityOfElementLocated(belcartLogo));
+        return belcart.isDisplayed();
+    }
+
+    @Step("Выбрать опцию '{optionName}' в выпадающем меню")
     public void selectOption(String optionName) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement popupMenu = wait.until(ExpectedConditions.elementToBeClickable(menu));
@@ -161,6 +180,7 @@ public class OnlinePaymentPage {
         return getPlaceholder(fieldEmail4);
     }
 
+    @Step("Нажать на ссылку 'Подробнее о сервисе'")
     public void clickLink() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement link = wait.until(ExpectedConditions.elementToBeClickable(detailsLink));
@@ -173,6 +193,7 @@ public class OnlinePaymentPage {
         return header.getText();
     }
 
+    @Step("Заполнить форму с номером телефона '{phone}' и суммой '{sum}' и нажать 'Продолжить'")
     public void fillFormAndContinue(String phone, String sum) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement phoneNumber = wait.until(ExpectedConditions.visibilityOfElementLocated(fieldPhoneNumber));
@@ -188,54 +209,82 @@ public class OnlinePaymentPage {
         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frame));
     }
 
+    @Step("Получить текст заголовка во фрейме")
     public String getFrameTitleText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement title = wait.until(ExpectedConditions.visibilityOfElementLocated(frameTitle));
         return title.getText();
     }
 
+    @Step("Получить текст кнопки оплаты во фрейме")
     public String getButtonPayText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(buttonPay));
         return button.getText();
     }
 
+    @Step("Получить отображаемый номер телефона во фрейме")
     public String getPhoneNumberText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement numberText = wait.until(ExpectedConditions.visibilityOfElementLocated(phoneNumberText));
         return numberText.getText();
     }
 
+    @Step("Получить текст поля 'Номер карты' во фрейме")
     public String getCardNumberText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement cardNumberText = wait.until(ExpectedConditions.visibilityOfElementLocated(cardNumber));
         return cardNumberText.getText();
     }
 
+    @Step("Получить текст поля 'Срок действия' во фрейме")
     public String getExpDateFieldText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement expDateFieldText = wait.until(ExpectedConditions.visibilityOfElementLocated(expDateField));
         return expDateFieldText.getText();
     }
 
+    @Step("Получить текст поля 'CVC' во фрейме")
     public String getCvcFieldText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement cvcFieldText = wait.until(ExpectedConditions.visibilityOfElementLocated(cvcField));
         return cvcFieldText.getText();
     }
 
+    @Step("Получить текст поля 'Имя и фамилия на карте' во фрейме")
     public String getNameFamilyFieldText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement nameFamilyFieldText = wait.until(ExpectedConditions.visibilityOfElementLocated(nameFamilyField));
         return nameFamilyFieldText.getText();
     }
 
-    public boolean areAllPaymentLogosDisplayed() {
+    @Step("Проверить отображение логотипа Visa во фрейме")
+    public boolean isPaymentLogoVisaDisplayedInFrame() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(visa)).isDisplayed()
-                && wait.until(ExpectedConditions.visibilityOfElementLocated(masterCard)).isDisplayed()
-                && wait.until(ExpectedConditions.visibilityOfElementLocated(belcart)).isDisplayed()
-                && wait.until(ExpectedConditions.visibilityOfElementLocated(maestro)).isDisplayed()
-                && wait.until(ExpectedConditions.visibilityOfElementLocated(mir)).isDisplayed();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(visa)).isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа MasterCard во фрейме")
+    public boolean isPaymentLogoMasterCardDisplayedInFrame() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(masterCard)).isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа Belcart во фрейме")
+    public boolean isPaymentLogoBelcartDisplayedInFrame() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(belcart)).isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа Maestro во фрейме")
+    public boolean isPaymentLogoMaestroDisplayedInFrame() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(maestro)).isDisplayed();
+    }
+
+    @Step("Проверить отображение логотипа Mir во фрейме")
+    public boolean isPaymentLogoMirDisplayedInFrame() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(mir)).isDisplayed();
     }
 }
--- src/test/java/com/mts/tests/OnlinePaymentTest.java
+++ src/test/java/com/mts/tests/OnlinePaymentTest.java
@@ -38,29 +38,78 @@ public class OnlinePaymentTest {
 
     @Test
     @Story("Проверка отображения логотипов платежных систем")
-    void testArePaymentLogosDisplayed() {
-        assertTrue(paymentPage.arePaymentLogosDisplayed(),
-                " Все логотипы платёжных систем должны отображаться на странице");
+    @DisplayName("Проверка логотипа Visa")
+    void testIsPaymentLogoVisaDisplayed() {
+        assertTrue(paymentPage.isPaymentLogoVisaDisplayed(),
+                " Логотип платёжной системы Visa должен отображаться на странице");
     }
 
     @Test
-    @Story("Проверка надписей в незаполненных полях каждого варианта оплаты услуг: услуги связи, домашний интернет, рассрочка, задолженность")
-    void testSelectOption() {
+    @Story("Проверка отображения логотипов платежных систем")
+    @DisplayName("Проверка логотипа VerifiedByVisa")
+    void testIsPaymentLogoVerifiedByVisaDisplayed() {
+        assertTrue(paymentPage.isPaymentLogoVerifiedByVisaDisplayed(),
+                " Логотип платёжной системы VerifiedByVisa должен отображаться на странице");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем")
+    @DisplayName("Проверка логотипа MasterCard")
+    void testIsPaymentLogoMasterCardDisplayed() {
+        assertTrue(paymentPage.isPaymentLogoMasterCardDisplayed(),
+                " Логотип платёжной системы MasterCard должен отображаться на странице");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем")
+    @DisplayName("Проверка логотипа MasterCard Secure Code")
+    void testIsPaymentLogoMasterCardSecureCodeDisplayed() {
+        assertTrue(paymentPage.isPaymentLogoMasterCardSecureCodeDisplayed(),
+                " Логотип платёжной системы MasterCardSecureCode должен отображаться на странице");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем")
+    @DisplayName("Проверка логотипа Belcart")
+    void testIsPaymentLogoBelcartDisplayed() {
+        assertTrue(paymentPage.isPaymentLogoBelcartDisplayed(),
+                " Логотип платёжной системы Belcart должен отображаться на странице");
+    }
+
+    @Test
+    @Story("Проверка надписей в незаполненных полях")
+    @DisplayName("Раздел: Услуги связи")
+    void testSelectOption1() {
         paymentPage.selectOption("Услуги связи");
         assertEquals("Номер телефона", paymentPage.getFieldPhoneNumberPlaceholder(), "Надпись не совпадает");
         assertEquals("Сумма", paymentPage.getFieldSumPlaceholder(), "Надпись не совпадает");
         assertEquals("E-mail для отправки чека", paymentPage.getFieldEmailPlaceholder(), "Надпись не совпадает");
+    }
 
+    @Test
+    @Story("Проверка надписей в незаполненных полях")
+    @DisplayName("Раздел: Домашний интернет")
+    void testSelectOption2() {
         paymentPage.selectOption("Домашний интернет");
         assertEquals("Номер абонента", paymentPage.getFieldPhoneNumber2Placeholder(), "Надпись не совпадает");
         assertEquals("Сумма", paymentPage.getFieldSum2Placeholder(), "Надпись не совпадает");
         assertEquals("E-mail для отправки чека", paymentPage.getFieldEmail2Placeholder(), "Надпись не совпадает");
+    }
 
+    @Test
+    @Story("Проверка надписей в незаполненных полях")
+    @DisplayName("Раздел: Рассрочка")
+    void testSelectOption3() {
         paymentPage.selectOption("Рассрочка");
         assertEquals("Номер счета на 44", paymentPage.getFieldAccountNumberPlaceholder(), "Надпись не совпадает");
         assertEquals("Сумма", paymentPage.getFieldSum3Placeholder(), "Надпись не совпадает");
         assertEquals("E-mail для отправки чека", paymentPage.getFieldEmail3Placeholder(), "Надпись не совпадает");
+    }
 
+    @Test
+    @Story("Проверка надписей в незаполненных полях")
+    @DisplayName("Раздел: Задолженность")
+    void testSelectOption4() {
         paymentPage.selectOption("Задолженность");
         assertEquals("Номер счета на 2073", paymentPage.getFieldAccountNumber2Placeholder(), "Надпись не совпадает");
         assertEquals("Сумма", paymentPage.getFieldSum4Placeholder(), "Надпись не совпадает");
@@ -76,21 +125,102 @@ public class OnlinePaymentTest {
     }
 
     @Test
-    @Story("Проверка заполнения формы и отображения суммы, телефона и полей карты в окне оплаты")
-    void testFillFormAndContinue() {
+    @Story("Проверка заполнения формы и отображения заголовков во фрейме")
+    @DisplayName("Отображение суммы")
+    void testFrameShowsCorrectAmount() {
         paymentPage.fillFormAndContinue("297777777", "100");
         assertTrue(paymentPage.getFrameTitleText().contains("100.00 BYN"),
                 "Заголовок должен быть'100.00 BYN'");
+    }
+
+    @Test
+    @Story("Проверка заполнения формы и отображения заголовков во фрейме")
+    @DisplayName("Отображение номера телефона")
+    void testFramePayButtonText() {
+        paymentPage.fillFormAndContinue("297777777", "100");
         String buttonText = paymentPage.getButtonPayText().trim().replaceAll("\\s+", " ");
         assertTrue(buttonText.contains("Оплатить 100.00 BYN"),
                 "Надпись на кнопке не совпадает");
+    }
+
+    @Test
+    @Story("Проверка заполнения формы и отображения заголовков во фрейме")
+    @DisplayName("Отображение суммы на кнопке оплаты")
+    void testFramePhoneNumber() {
+        paymentPage.fillFormAndContinue("297777777", "100");
         assertTrue(paymentPage.getPhoneNumberText().contains("297777777"), "Номер телефона не совпадает");
+    }
+
+    @Test
+    @Story("Проверка отображения надписей в незаполненных полях для ввода реквизитов карты")
+    @DisplayName("Поле: Номер карты")
+    void testGetCardNumberText() {
+        paymentPage.fillFormAndContinue("297777777", "100");
         assertTrue(paymentPage.getCardNumberText().contains("Номер карты"), "Надпись не совпадает");
+    }
+
+    @Test
+    @Story("Проверка отображения надписей в незаполненных полях для ввода реквизитов карты")
+    @DisplayName("Поле: Срок действия")
+    void testGetExpDateFieldText() {
+        paymentPage.fillFormAndContinue("297777777", "100");
         assertTrue(paymentPage.getExpDateFieldText().contains("Срок действия"), "Надпись не совпадает");
+    }
+
+    @Test
+    @Story("Проверка отображения надписей в незаполненных полях для ввода реквизитов карты")
+    @DisplayName("Поле: CVC")
+    void testGetCvcFieldText() {
+        paymentPage.fillFormAndContinue("297777777", "100");
         assertTrue(paymentPage.getCvcFieldText().contains("CVC"), "Надпись не совпадает");
+    }
+
+    @Test
+    @Story("Проверка отображения надписей в незаполненных полях для ввода реквизитов карты")
+    @DisplayName("Поле: Имя и фамилия на карте")
+    void testGetNameFamilyFieldText() {
+        paymentPage.fillFormAndContinue("297777777", "100");
         assertTrue(paymentPage.getNameFamilyFieldText().contains("Имя и фамилия на карте"), "Надпись не совпадает");
-        assertTrue(paymentPage.areAllPaymentLogosDisplayed(),
-                " Все логотипы платёжных систем должны отображаться на странице");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем во фрейме")
+    @DisplayName("Проверка логотипа Visa")
+    void testIsPaymentLogoVisaDisplayedInFrame() {
+        paymentPage.fillFormAndContinue("297777777", "100");
+        assertTrue(paymentPage.isPaymentLogoVisaDisplayedInFrame(), "Логотип Visa не отображается");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем во фрейме")
+    @DisplayName("Проверка логотипа MasterCard")
+    void testIsPaymentLogoMasterCardDisplayedInFrame() {
+        paymentPage.fillFormAndContinue("297777777", "100");
+        assertTrue(paymentPage.isPaymentLogoMasterCardDisplayedInFrame(), "Логотип MasterCard не отображается");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем во фрейме")
+    @DisplayName("Проверка логотипа Belcart")
+    void testIsPaymentLogoBelcartDisplayedInFrame() {
+        paymentPage.fillFormAndContinue("297777777", "100");
+        assertTrue(paymentPage.isPaymentLogoBelcartDisplayedInFrame(), "Логотип Belcart не отображается");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем во фрейме")
+    @DisplayName("Проверка логотипа Maestro")
+    void testIsPaymentLogoMaestroDisplayedInFrame() {
+        paymentPage.fillFormAndContinue("297777777", "100");
+        assertTrue(paymentPage.isPaymentLogoMaestroDisplayedInFrame(), "Логотип Maestro не отображается");
+    }
+
+    @Test
+    @Story("Проверка отображения логотипов платежных систем во фрейме")
+    @DisplayName("Проверка логотипа Mir")
+    void testIsPaymentLogoMirDisplayedInFrame() {
+        paymentPage.fillFormAndContinue("297777777", "100");
+        assertTrue(paymentPage.isPaymentLogoMirDisplayedInFrame(), "Логотип Mir не отображается");
     }
 
     @AfterEach
